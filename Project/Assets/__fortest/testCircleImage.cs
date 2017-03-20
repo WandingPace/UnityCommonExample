a014f3f38b8a95d47b134c2cf2097c2471b572ce
leap <1007416747@qq.com>
2017-03-20 19:32:53 +0800

    [UIControl]add circle ui

---
+++ Project/Assets/__fortest/testCircleImage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class testCircleImage : Image {
+
+    [SerializeField]
+    private int curSegments;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    protected override void OnPopulateMesh(VertexHelper toFill)
+    {
+        base.OnPopulateMesh(toFill);
+
+    }
+}
